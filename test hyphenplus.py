26f197d0fd7481d04178c8d74ebf5a9bc933b83a
phai <48747872+tonpai1007@users.noreply.github.com>
2024-03-06 17:11:56 +0700

    Add files via upload

---
+++ test hyphenplus.py
@@ -0,0 +1,12 @@
+def cansum(nums,target):
+    seen=set()
+    for num in nums:
+        complement = target-num
+        if complement in seen:
+            return True
+        seen.add(num)
+    return False
+
+nums = list(map(int,input("enter number").split()))
+target = int(input("enter target"))
+print(cansum(nums,target))  
